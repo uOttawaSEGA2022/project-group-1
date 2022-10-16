033d6ccfc7752bf283aa64e59cc3540b55f6175f
mrhappynicolas2 <nberu056@uottawa.ca>
2022-10-16 17:39:54 -0400

    lab4

---
+++ Labs/Lab4/Nicolas/Lab4/app/src/main/java/com/example/lab4/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.lab4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void OnOpenInGoogleMaps(View view) {
+        EditText teamAddress = (EditText) findViewById(R.id.teamName);
+
+        // Create a URI from an intent string.
+        Uri gmmIntentUri = Uri.parse("geo:0,0?q=" + teamAddress.getText());
+
+        // Create an Intent from gmmIntentUri. Set the action to ACTION_VIEW
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+
+        // Make the Intent explicit by setting the Google Maps package
+        mapIntent.setPackage("com.google.android.apps.maps");
+
+        // Attempt to start an activity that can handle the Intent
+        startActivity(mapIntent);
+    }
+
+    public void OnSetAvatarButton(View view) {
+        Intent intent = new Intent(getApplicationContext(), MainActivity2.class);
+        startActivityForResult(intent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_CANCELED) return;
+
+        //Getting the Avatar Image we show to our users
+        ImageView avatarImage = (ImageView) findViewById(R.id.avatarImage);
+
+        //Figuring out the correct image
+        String drawableName = "ic_logo_00";
+        switch (data.getIntExtra("imageID",R.id.teamid00)) {
+            case R.id.teamid00:
+                System.out.println("test0");
+                drawableName = "ic_logo_00";
+                break;
+            case R.id.teamid01:
+                drawableName = "ic_logo_01";
+                System.out.println("test1");
+                break;
+            case R.id.teamid02:
+                drawableName = "ic_logo_02";
+                System.out.println("test2");
+                break;
+            case R.id.teamid03:
+                drawableName = "ic_logo_03";
+                System.out.println("test#");
+                break;
+            case R.id.teamid04:
+                drawableName = "ic_logo_04";
+                System.out.println("test4");
+                break;
+            case R.id.teamid05:
+                drawableName = "ic_logo_05";
+                System.out.println("test5");
+                break;
+            default:
+                drawableName = "ic_logo_00";
+                System.out.println("test00");;
+                break;
+        }
+        int resID = getResources().getIdentifier(drawableName, "drawable", getPackageName());
+        avatarImage.setImageResource(resID);
+    }
+
+}
---
+++ Labs/Lab4/Nicolas/Lab4/app/src/main/java/com/example/lab4/MainActivity2.java
@@ -0,0 +1,31 @@
+package com.example.lab4;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    public void SetTeamIcon(View view) {
+        //Creating a Return intent to pass to the Main Activity
+        Intent returnIntent = new Intent();
+
+        //Figuring out which image was clicked
+        ImageView selectedImage = (ImageView) view;
+
+        //Adding stuff to the return intent
+        returnIntent.putExtra("imageID", selectedImage.getId());
+        setResult(RESULT_OK, returnIntent);
+
+        //Finishing Activity and return to main screen!
+        finish();
+    }
+}
