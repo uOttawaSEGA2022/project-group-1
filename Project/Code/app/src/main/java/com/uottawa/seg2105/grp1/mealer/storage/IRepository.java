b11b08c72dec549f2a54c828e249c75a0aff8860
Samuel Pierre-Louis <spier033@uottawa.ca>
2022-10-15 20:48:03 -0400

    IRepository can now throw RepositoryRequestException

--- Project/Code/app/src/main/java/com/uottawa/seg2105/grp1/mealer/storage/IRepository.java
+++ Project/Code/app/src/main/java/com/uottawa/seg2105/grp1/mealer/storage/IRepository.java
@@ -17,8 +17,10 @@ public interface IRepository {
      * @param cls The Entity subclass to search
      * @param id The id of the element to verify the existence of.
      * @return true if there is such an element in permanent storage, false otherwise
+     *
+     * @exception RepositoryRequestException if data could not be fetched
      */
-    public <T extends Entity> boolean hasId(Class<T> cls, String id);
+    public <T extends Entity> boolean hasId(Class<T> cls, String id) throws RepositoryRequestException;
 
     /**
      * Deserialises an entity by fetching it by ID.
@@ -26,8 +28,10 @@ public interface IRepository {
      * @param cls The Entity subclass to search
      * @param id The id of the element to fetch.
      * @return The entity that was deserialised, or null if no entity with the specified ID exists
+     *
+     * @exception RepositoryRequestException if data could not be fetched
      */
-    public <T extends Entity> T getById(Class<T> cls, String id);
+    public <T extends Entity> T getById(Class<T> cls, String id) throws RepositoryRequestException;
 
     /**
      * Serialises an entity and stores it in persistent storage.
@@ -37,9 +41,10 @@ public interface IRepository {
      *
      * @param cls The Entity subclass to search
      * @param entity The entity of type T that needs to be created in persistent storage
-     * @return true if the entity was created or overwrote an element in permanent storage
+     *
+     * @exception RepositoryRequestException if data could not be added or overwritten
      */
-    public <T extends Entity> boolean set(Class<T> cls, T entity);
+    public <T extends Entity> void set(Class<T> cls, T entity) throws RepositoryRequestException;
 
     /**
      * Updates the fields of the entity with ID {@code id} with new values specified by the {@code prop} map
@@ -50,9 +55,10 @@ public interface IRepository {
      * @param cls The Entity subclass to search
      * @param id the id of the Entity to update
      * @param props the properties names and values to update
-     * @return true if the update was successful, false otherwise
+     *
+     * @exception RepositoryRequestException if data could not be updated
      */
-    public <T extends Entity> boolean update(Class<T> cls, String id, Map<String, Object> props);
+    public <T extends Entity> void update(Class<T> cls, String id, Map<String, Object> props) throws RepositoryRequestException;
 
     /**
      * Deletes the entity from permanent storage.
@@ -61,14 +67,17 @@ public interface IRepository {
      *
      * @param cls The Entity subclass to search
      * @param entity The entity the delete
-     * @return true if the entity was removed or didn't exist yet
+     *
+     * @exception RepositoryRequestException if data could not be deleted
      */
-    public <T extends Entity> boolean delete(Class<T> cls, T entity);
+    public <T extends Entity> void delete(Class<T> cls, T entity) throws RepositoryRequestException;
 
     /**
      * @return All entities of the specified type in permanent storage
+     *
+     * @exception RepositoryRequestException if data could not be fetched
      */
-    public <T extends Entity> List<T> list(Class<T> cls);
+    public <T extends Entity> List<T> list(Class<T> cls) throws RepositoryRequestException;
 
     /**
      * Queries permanent storage for all entities matching the condition specified by the predicate.
@@ -76,6 +85,8 @@ public interface IRepository {
      * @param cls The Entity subclass to search
      * @param predicate The predicate representing the condition of the query
      * @return All entities who match the predicate
+     *
+     * @exception RepositoryRequestException if data could not be fetched
      */
-    public <T extends Entity> List<T> query(Class<T> cls, Predicate<T> predicate);
+    public <T extends Entity> List<T> query(Class<T> cls, Predicate<T> predicate) throws RepositoryRequestException;
 }
---
+++ Project/Code/app/src/main/java/com/uottawa/seg2105/grp1/mealer/storage/RepositoryRequestException.java
@@ -0,0 +1,6 @@
+package com.uottawa.seg2105.grp1.mealer.storage;
+
+/**
+ * An exception thrown whenever an error eccors within a Repository request
+ */
+public class RepositoryRequestException extends Exception {}
