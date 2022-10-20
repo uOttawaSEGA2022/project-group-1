4f5d729a44eb4e66f629b898f41cd6e2ca2276ed
Shyan Arul <shyana@Shyans-MacBook-Air.local>
2022-10-20 14:47:48 -0400

    updated Lab

--- Labs/Lab4/Shyan/ProfileManager/app/src/main/java/com/example/profilemanager/MainActivity.java
+++ Labs/Lab4/Shyan/ProfileManager/app/src/main/java/com/example/profilemanager/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.profilemanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,62 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void OnOpenInGoogleMaps(View view) {
+        EditText teamAddress = (EditText) findViewById(R.id.teamAddress);
+
+        // Create a URI from an intent string.
+        Uri gmmIntentUri = Uri.parse("geo:0,0?q=" + teamAddress.getText());
+
+        // Create an Intent from gmmIntentUri. Set the action to ACTION_VIEW
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+
+        // Make the Intent explicit by setting the Google Maps package
+        mapIntent.setPackage("com.google.android.apps.maps");
+
+        // Attempt to start an activity that can handle the Intent
+        startActivity(mapIntent);
+    }
+
+    public void OnSetAvatarButton(View view) {
+        Intent intent = new Intent(getApplicationContext(), AvatarActivity.class);
+        startActivityForResult(intent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_CANCELED) return;
+
+        //Getting the Avatar Image we show to our users
+        ImageView avatarImage = (ImageView) findViewById(R.id.imageView);
+
+        //Figuring out the correct image
+        String drawableName = "ic_logo_00";
+        switch (data.getIntExtra("imageID",R.id.team00)) {
+            case R.id.team00:
+                drawableName = "ic_logo_00";
+                break;
+            case R.id.team01:
+                drawableName = "ic_logo_01";
+                break;
+            case R.id.team02:
+                drawableName = "ic_logo_02";
+                break;
+            case R.id.team03:
+                drawableName = "ic_logo_03";
+                break;
+            case R.id.team04:
+                drawableName = "ic_logo_04";
+                break;
+            case R.id.team05:
+                drawableName = "ic_logo_05";
+                break;
+            default:
+                drawableName = "ic_logo_00";
+                break;
+        }
+        int resID = getResources().getIdentifier(drawableName, "drawable", getPackageName());
+        avatarImage.setImageResource(resID);
     }
 }
