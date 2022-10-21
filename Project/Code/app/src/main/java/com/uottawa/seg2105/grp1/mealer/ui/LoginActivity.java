1f35deb15ed19d0f3d2625eea9d9e1fd87834895
justin-zz <113710163+justin-zz@users.noreply.github.com>
2022-10-21 15:40:22 -0400

    removed an unused button in LoginActivity

--- Project/Code/app/src/main/java/com/uottawa/seg2105/grp1/mealer/ui/LoginActivity.java
+++ Project/Code/app/src/main/java/com/uottawa/seg2105/grp1/mealer/ui/LoginActivity.java
@@ -59,17 +59,6 @@ public class LoginActivity extends AppCompatActivity {
 
     private static final String TAG = "LoginActivity";
 
-    //Just a test method; you may remove it
-    //Adds a user to the collection "users" to the Cloud Firestore DB
-    public void testBtn(View view) {
-
-        //Get instance of our DB
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        FirestoreTest test = new FirestoreTest(db);
-
-        //Adds a user to the DB
-        test.addUser(view);
-    }
     //btn that opens a loginpage class
     public void btnLoginPage(View view){
         System.out.println("loginpage");
