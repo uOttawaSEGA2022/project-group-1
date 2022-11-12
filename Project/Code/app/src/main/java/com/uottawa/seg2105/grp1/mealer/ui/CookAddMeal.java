63d13f3b1fc80f2324187b4eec79ba63b010b4f4
mrhappynicolas2 <nberu056@uottawa.ca>
2022-11-11 21:25:36 -0500

    Everything works except the user

--- Project/Code/app/src/main/java/com/uottawa/seg2105/grp1/mealer/ui/CookAddMeal.java
+++ Project/Code/app/src/main/java/com/uottawa/seg2105/grp1/mealer/ui/CookAddMeal.java
@@ -5,22 +5,243 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.uottawa.seg2105.grp1.mealer.R;
 import com.uottawa.seg2105.grp1.mealer.lib.Utility;
+import com.uottawa.seg2105.grp1.mealer.model.ClientRole;
+import com.uottawa.seg2105.grp1.mealer.model.Complaint;
+import com.uottawa.seg2105.grp1.mealer.model.Meal;
+import com.uottawa.seg2105.grp1.mealer.model.MealerSystem;
+import com.uottawa.seg2105.grp1.mealer.model.User;
+import com.uottawa.seg2105.grp1.mealer.model.UserAlreadyExistsException;
+import com.uottawa.seg2105.grp1.mealer.storage.IRepository;
+import com.uottawa.seg2105.grp1.mealer.storage.RepositoryRequestException;
+
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class CookAddMeal extends AppCompatActivity {
+
+    //this is for the multiple choice
+    TextView cuisineTypeView;
+    TextView mealTypeView;
+    boolean[] selectedCuisine;
+    boolean[] selectedMeal;
+    ArrayList<Integer> cuisineList = new ArrayList<>();
+    ArrayList<Integer> mealList = new ArrayList<>();
+    String[] cuisineTypeArray = {"Italian", "Thai", "Indian", "Other"};
+    String[] mealTypeArray = {"Breakfast", "Lunch", "Dinner", "Other"};
+    private String cookID;
+    User user;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cook_add_meal);
-    }
 
-    private boolean isValidItem(EditText itemName, EditText mealType,
-                                  EditText cuisineType, EditText itemAllergens,
-                                  EditText itemIngrediants, EditText itemDescription) {
+        cuisineTypeView = findViewById(R.id.cuisineTypeView);
+        mealTypeView = findViewById(R.id.mealTypeView);
+        selectedCuisine = new boolean[mealTypeArray.length];
+
+        //gets cook id
+        if (savedInstanceState == null) {
+            Bundle extras = getIntent().getExtras();
+            if (extras == null) {
+                cookID = "cook@email.com";
+            } else {
+                cookID = extras.getString("cookId");
+            }
+        } else {
+            cookID = (String) savedInstanceState.getSerializable("cookId");
+        }
+
+        //TODO: HERE
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    IRepository rep = MealerSystem.getSystem().getRepository();
+                    user = rep.getById(User.class, cookID);
+                    System.out.println(user.getRole());
+                    System.out.println(user.getEmail());
+                    System.out.println("next is to see outside");
+
+                } catch (RepositoryRequestException e) {
+                    runOnUiThread(() -> {
+                        Toast.makeText(getApplicationContext(), "Could not get data from repository.", Toast.LENGTH_LONG).show();
+                    });
+                }
+            }
+
+        }.start();
+        //TODO:END
+
+        System.out.println("Now outside");
+
+        System.out.println(user);
+        //System.out.println(user.getEmail());
+
+
+        //Cuisine multiple choice
+        cuisineTypeView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // Initialize alert dialog
+                AlertDialog.Builder builder = new AlertDialog.Builder(CookAddMeal.this);
+
+                // set title
+                builder.setTitle("Select Cuisine Type");
+
+                // set dialog non cancelable
+                builder.setCancelable(false);
+
+                builder.setMultiChoiceItems(cuisineTypeArray, selectedCuisine, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i, boolean b) {
+                        // check condition
+                        if (b) {
+                            // when checkbox selected
+                            // Add position  in lang list
+                            cuisineList.add(i);
+                            // Sort array list
+                            Collections.sort(cuisineList);
+                        } else {
+                            // when checkbox unselected
+                            // Remove position from langList
+                            cuisineList.remove(Integer.valueOf(i));
+                        }
+                    }
+                });
+
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        // Initialize string builder
+                        StringBuilder stringBuilder = new StringBuilder();
+                        // use for loop
+                        for (int j = 0; j < cuisineList.size(); j++) {
+                            // concat array value
+                            stringBuilder.append(cuisineTypeArray[cuisineList.get(j)]);
+                            // check condition
+                            if (j != cuisineList.size() - 1) {
+                                // When j value  not equal
+                                // to lang list size - 1
+                                // add comma
+                                stringBuilder.append(", ");
+                            }
+                        }
+                        // set text on textView
+                        cuisineTypeView.setText(stringBuilder.toString());
+                        //cuisineTypeView.setText("test");
+                        System.out.println("okkakyyy");
+                    }
+                });
+
+                //put Cancel on the left side (Neutral)
+                builder.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        // dismiss dialog
+                        dialogInterface.dismiss();
+                    }
+                });
+
+                // show dialog
+                builder.show();
+            }
+        });
+
+
+        //Meal Type multiple choice
+        mealTypeView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // Initialize alert dialog
+                AlertDialog.Builder builder = new AlertDialog.Builder(CookAddMeal.this);
+
+                // set title
+                builder.setTitle("Select Meal Type");
+
+                // set dialog non cancelable
+                builder.setCancelable(false);
+
+                builder.setMultiChoiceItems(mealTypeArray, selectedMeal, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i, boolean b) {
+                        // check condition
+                        if (b) {
+                            // when checkbox selected
+                            // Add position  in lang list
+                            mealList.add(i);
+                            // Sort array list
+                            Collections.sort(mealList);
+                        } else {
+                            // when checkbox unselected
+                            // Remove position from langList
+                            mealList.remove(Integer.valueOf(i));
+                        }
+                    }
+                });
+
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        // Initialize string builder
+                        StringBuilder stringBuilder = new StringBuilder();
+                        // use for loop
+                        for (int j = 0; j < mealList.size(); j++) {
+                            // concat array value
+                            stringBuilder.append(mealTypeArray[mealList.get(j)]);
+                            // check condition
+                            if (j != mealList.size() - 1) {
+                                // When j value  not equal
+                                // to lang list size - 1
+                                // add comma
+                                stringBuilder.append(", ");
+                            }
+                        }
+                        // set text on textView
+                        mealTypeView.setText(stringBuilder.toString());
+                        //cuisineTypeView.setText("test");
+                        System.out.println("okkakyyy");
+                    }
+                });
+
+                //put Cancel on the left side (Neutral)
+                builder.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        // dismiss dialog
+                        dialogInterface.dismiss();
+                    }
+                });
+
+                // show dialog
+                builder.show();
+            }
+        });
+    }
+    //end of multiple choice
+
+
+    private boolean isValidItem(EditText itemName, TextView mealType, TextView cuisineType,
+                                EditText itemAllergens, EditText itemPrice,
+                                EditText itemIngrediants, EditText itemDescription) {
         boolean result = true;
 
 
@@ -28,20 +249,26 @@ public class CookAddMeal extends AppCompatActivity {
             itemName.setError("Item name required");
             result = false;
         }
-
-        if (Utility.isEmpty(mealType)) {
+        System.out.println(mealType.getText());
+        if (mealType.getText() == "") {
             mealType.setError("Meal type required");
             result = false;
+
         }
 
 
-        if (Utility.isEmpty(cuisineType)) {
+        if (cuisineType.getText() == "") {
             cuisineType.setError("Cuisine type required");
             result = false;
         }
 
         if (Utility.isEmpty(itemAllergens)) {
             itemAllergens.setError("List of allergens required");
+            result = false;
+        }
+
+        if (Utility.isEmpty(itemPrice)) {
+            itemPrice.setError("List of allergens required");
             result = false;
         }
 
@@ -57,21 +284,54 @@ public class CookAddMeal extends AppCompatActivity {
 
         return result;
     }
-    public void btnAddItem(View view){
+
+    public void btnAddItem(View view) {
 
 
         EditText menuName = findViewById(R.id.fieldMenuName);
-        EditText mealType = findViewById(R.id.fieldMealType);
-        EditText cuisineType = findViewById(R.id.fieldCuisineType);
+        TextView mealType = findViewById(R.id.mealTypeView);
+        TextView cuisineType = findViewById(R.id.cuisineTypeView);
         EditText itemAllergens = findViewById(R.id.fieldAllergens);
         EditText itemIngrediants = findViewById(R.id.fieldIngredients);
         EditText itemDescription = findViewById(R.id.fieldDescription);
+        EditText itemPrice = findViewById(R.id.fieldPrice);
 
-        boolean valid = isValidItem(menuName, mealType, cuisineType, itemAllergens,
-                 itemIngrediants, itemDescription);
+        User user = new User();
+        //user.getByEmail(cookID);
+        //System.out.println(user);
+        //System.out.println(user.getEmail());
+        //System.out.println(user.getId());
+
+
+
+        boolean valid = isValidItem(menuName, mealType, cuisineType, itemAllergens, itemPrice,
+                itemIngrediants, itemDescription);
         if (valid) {
-            //TODO: add the item to the database (for that cook)
-            finish();
-        }
+            System.out.println(valid);
+
+
+            view.setEnabled(false);
+            new Thread() {
+                @Override
+                public void run() {
+                    try {
+                        Meal meal = new Meal();
+                        meal.createMeal(
+                                menuName.getText().toString(), mealType.getText().toString(),
+                                cuisineType.getText().toString(), itemIngrediants.getText().toString(),
+                                itemAllergens.getText().toString(), Integer.parseInt(itemPrice.getText().toString()),
+                                itemDescription.getText().toString(), user, true);
+
+                    } catch (RepositoryRequestException e) {
+                        runOnUiThread(() -> {
+                            Toast.makeText(CookAddMeal.this, "An error occurred.", Toast.LENGTH_SHORT).show();
+                            view.setEnabled(true);
+                        });
+
+                    }
+                }
+            }.start();
+            //TODO: END HERE
+        } finish();
     }
 }
