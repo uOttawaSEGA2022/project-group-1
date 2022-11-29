c0c593ac224c2f811bfdcb2991aeb32febdb80ec
mrhappynicolas2 <nberu056@uottawa.ca>
2022-11-29 15:09:21 -0500

    Client home

--- Project/Code/app/src/main/java/com/uottawa/seg2105/grp1/mealer/ui/ClientHome.java
+++ Project/Code/app/src/main/java/com/uottawa/seg2105/grp1/mealer/ui/ClientHome.java
@@ -5,18 +5,48 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.uottawa.seg2105.grp1.mealer.R;
 import com.uottawa.seg2105.grp1.mealer.lib.Utility;
 import com.uottawa.seg2105.grp1.mealer.model.MealerSystem;
+import com.uottawa.seg2105.grp1.mealer.model.User;
 
 public class ClientHome extends AppCompatActivity {
+    private TextView txtWelcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_home);
+
+        User user = MealerSystem.getSystem().getCurrentUser();
+        String fullname = user.getFirstName() + " " + user.getLastName();
+
+        txtWelcome = (TextView) findViewById(R.id.txtWelcome);
+        txtWelcome.setText("Welcome, " + fullname);
     }
+
+
+    //TODO: change deubgmode to the name of the activity
+    public void btnOrderHistory(View view){
+        Intent intent = new Intent(getApplicationContext(), DebugMode.class);
+        intent.putExtra("cookId", MealerSystem.getSystem().getCurrentUser().getId());
+        startActivity(intent);
+    }
+
+    public void btnSearch(View view){
+        Intent intent = new Intent(getApplicationContext(), DebugMode.class);
+        intent.putExtra("cookId", MealerSystem.getSystem().getCurrentUser().getId());
+        startActivity(intent);
+    }
+
+    public void btnPayment(View view){
+        Intent intent = new Intent(getApplicationContext(), DebugMode.class);
+        intent.putExtra("cookId", MealerSystem.getSystem().getCurrentUser().getId());
+        startActivity(intent);
+    }
+
 
     public void onLogOff(View view) {
         new Thread() {
